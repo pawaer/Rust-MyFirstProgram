88a71228cb4c07c7f566f399cf4b468bc6661f57
Patrick Walberer <pawaer@t-online.de>
2022-08-19 21:58:09 +0200

    works in a look with random generator

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    println!("Guess a number!:");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let secretNumber = rand::thread_rng().gen_range(1..=100);
+    let mut entered_number = String::new();
 
-    let mut enteredNumber = String::new();
+    loop {
+        let mut entered_number = String::new();
 
-    io::stdin().read_line(&mut enteredNumber).expect("Failed To Read line");
+        println!("Guess a number!:");
 
-    println!("You entered {enteredNumber},");
-    println!("expected number {secretNumber},");
+        io::stdin().read_line(&mut entered_number).expect("Failed To Read line");
+
+        println!("You entered {entered_number}");
+        println!("expected number {secret_number}");
+
+        let entered_number: u32 = entered_number.trim().parse().expect("Please enter a number");
+
+        println!("You entered {entered_number}");
+
+
+        match entered_number.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => println!("Jackpot!"),
+        }
+    }
 }
