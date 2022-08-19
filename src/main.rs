1dcf80411542ebefd48ea12a966f7404812d1777
Patrick Walberer <pawaer@t-online.de>
2022-08-19 21:31:53 +0200

    implemented a random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess a number!:");
+
+    let secretNumber = rand::thread_rng().gen_range(1..=100);
 
     let mut enteredNumber = String::new();
 
     io::stdin().read_line(&mut enteredNumber).expect("Failed To Read line");
 
-    println!("You entered {enteredNumber}");
-
-
-    let x = 5;
-    let y = -12;
-
-    println!("You entered x {x} and y {}", y);
+    println!("You entered {enteredNumber},");
+    println!("expected number {secretNumber},");
 }
