fc06d74695b9614f4fe73880943167af721991b7
Patrick Walberer <pawaer@t-online.de>
2022-08-19 21:24:30 +0200

    basic hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number!:");
+
+    let mut enteredNumber = String::new();
+
+    io::stdin().read_line(&mut enteredNumber).expect("Failed To Read line");
+
+    println!("You entered {enteredNumber}");
+
+
+    let x = 5;
+    let y = -12;
+
+    println!("You entered x {x} and y {}", y);
 }
